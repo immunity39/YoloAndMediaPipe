26f98bf2890c4279fe5bac25fa320d6292d810e9
immun1ty <so_kaicyo@icloud.com>
2025-10-24 16:49:12 +0900

    train def main add

--- programs/hand_CollisionDetect/yolo11training.py
+++ programs/hand_CollisionDetect/yolo11training.py
@@ -1,7 +1,18 @@
 from ultralytics import YOLO
 
-# Load a model
-model = YOLO("yolo11n-pose.pt")  # load a pretrained model (recommended for training)
+def main():
+    # Load a model
+    model = YOLO("yolo11n-pose.pt")  # load a pretrained model (recommended for training)
 
-# Train the model
-results = model.train(data="hand-keypoints.yaml", epochs=100, imgsz=640)
+    # Train the model
+    results = model.train(
+        data="hand-keypoints.yaml",
+        epochs=30,
+        imgsz=640,
+        batch=8,
+        name="yolo11n-hand-pose",
+        device='cuda:0'
+    )
+
+if __name__ == "__main__":
+    main()
