b487233b148c9300ad9995e3d9552a78f4ca86ff
immun1ty <so_kaicyo@icloud.com>
2025-10-26 04:21:40 +0900

    max num hands 1 to 2

--- programs/Pipe/hand.py
+++ programs/Pipe/hand.py
@@ -8,7 +8,7 @@ def main():
 
     with mp_hands.Hands(
         static_image_mode=False,
-        max_num_hands=1,
+        max_num_hands=2,
         min_detection_confidence=0.5,
         min_tracking_confidence=0.5
     ) as hands:
